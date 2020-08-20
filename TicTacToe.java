612a58efb9e40639490e79d5cc48f81b69aab35c
Sam Chris <thesamchris@gmail.com>
2020-08-20 22:10:38 +0800

    print empty game board

---
+++ TicTacToe.java
@@ -0,0 +1,29 @@
+public class TicTacToe {
+    public static void main(String[] args) {
+        /* gameboard should look something like this
+        0 | 0 | 0
+        - + - + -
+        X | X | X
+        - + - + -
+        0 | 0 | 0
+        */
+        char[][] gameBoard = {
+            {' ', '|', ' ', '|', ' '}, 
+            {'+', '-', '+', '-', '+'}, 
+            {' ', '|', ' ', '|', ' '}, 
+            {'+', '-', '+', '-', '+'}, 
+            {' ', '|', ' ', '|', ' '},
+        };
+
+        printGameBoard(gameBoard);
+    }
+
+    public static void printGameBoard(char[][] gameBoard) {
+        for(char[] row : gameBoard) {
+            for(char c : row) {
+                System.out.print(c);
+            }
+            System.out.println(' ');
+        }
+    }
+}
